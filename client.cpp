3b11f82aa605c98a3a2d6022060cec1c779784b5
Araddajr <39871561+Araddajr@users.noreply.github.com>
2020-04-14 14:19:59 -0500

    Worked on project
    Built up project.

--- client.cpp
+++ client.cpp
@@ -1,8 +1,78 @@
 #include <string>
 #include <thread>
 #include <iostream>
+#include <sys/stat.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <string.h>
+#include <netdb.h>
 
-int main()
-{
-  std::cerr << "client is not implemented yet" << std::endl;
+using namespace std;
+
+int main(int argc, char *argv[]){
+    {
+        if (argc != 4) {  // Check number of command-line arguments
+            cerr << "ERROR: Incorrect number of arguments. <HOSTNAME> <PORT> <FILENAME> is required.\n";  //Print error if incorrect number of arguments
+            exit(EXIT_FAILURE); // Exit the program
+        }
+
+        char* host = argv[1];           // Get host from command-line
+        int pnum = atoi(argv[2]);       // Get port number from command-line
+        string file = argv[3];          // Get file name from command-line
+
+        if (pnum < 1024 || pnum > 65535) {  // Check port number validity
+            cerr << "ERROR: Incorrect port number. Port number must be between 1024 and 65535.\n";  //Print error string on invalid port
+            exit(EXIT_FAILURE);  // Exit the program
+        }
+
+        struct addrinfo hostAddr, *hostInfo, *p; // Create structure to hold host address information
+
+        memset(&hostAddr, 0, sizeof(hostAddr));     // Clear hostAddr
+
+        hostAddr.ai_socktype = SOCK_DGRAM;          // Set socket type
+        hostAddr.ai_family = AF_INET;               // Set address family
+        int check = getaddrinfo(host, NULL, &hostAddr, &hostInfo);
+
+        if (check)  // Check hostname
+        {
+            cerr << "ERROR: Hostname is not correct\n";     // Print error if hostname not correct
+            exit(EXIT_FAILURE);     // Exit the program
+        }
+
+        char hostName[256];
+        for (p = hostInfo; p != NULL; p = p->ai_next) {
+            getnameinfo(p->ai_addr, p->ai_addrlen, hostName, sizeof(hostName), NULL, 0, NI_NUMERICHOST);
+        }
+
+        freeaddrinfo(hostInfo);
+
+        int sockfd;     //Variable for socket
+        struct sockaddr_in clientAddr;
+
+        if ((sockfd = socket(PF_INET, SOCK_DGRAM, 0) < 0)) {    // Create UDP socket
+            cerr << "ERROR: Failed creating socket";            // Print error on creation fail
+            exit(EXIT_FAILURE);                                 // Exit the program
+        }
+
+        fcntl(sockfd, F_SETFL, O_NONBLOCK);                     // Set socket to non-blocking
+
+        clientAddr.sin_family = AF_INET;
+        clientAddr.sin_port = htons(pnum);
+        clientAddr.sin_addr.s_addr = inet_addr(hostName);
+        memset(clientAddr.sin_zero, '\0', sizeof(clientAddr.sin_zero));
+
+        socklen_t clientSize;
+
+        fd_set readfds;                             // File decriptors to check
+        FD_ZERO(&readfds);                          // Set file descriptors to zero
+        FD_SET(sockfd, &readfds);                   // Set the file descriptor 
+
+        clientSize = sizeof(clientAddr);              // Save size
+
+    }
+
 }
--- server.cpp
+++ server.cpp
@@ -3,12 +3,21 @@
 #include <iostream>
 #include <sys/stat.h>
 #include <signal.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <string.h>
+
 
 //using std::cerr; 
 using namespace std;
 
 void signal_quit(int quit);
 void signal_term(int term);
+
+#define TOTAL 524
 
 int main(int argc, char *argv[]) {
 
@@ -33,7 +42,35 @@ int main(int argc, char *argv[]) {
 
     signal(SIGQUIT, signal_quit); // Check for SIQQUIT
     signal(SIGTERM, signal_quit); // Check for SIGTERM
+
+    int sockfd;             // Variable for socket
+    char buffer[TOTAL];     // Total buffer size
+    struct sockaddr_in serverAddr, clientAddr;         // Structures used to store server and client addresses 
+
+    socklen_t addrSize, clientAddrSize;         // Store size of addresses
+
+    if ((sockfd = socket(PF_INET, SOCK_DGRAM, 0) < 0)) {    // Create UDP socket
+        cerr << "ERROR: Failed creating socket";            // Print error on creation fail
+        exit(EXIT_FAILURE);                                 // Exit the program
+    }
+
+    fcntl(sockfd, F_SETFL, O_NONBLOCK);                     // Set socket to non-blocking
+
+    serverAddr.sin_family = AF_INET;                                        // Set address family
+    serverAddr.sin_port = htons(pnum);                                      // Set port number
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);                         // Set address
+    memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
   
+    if (bind(sockfd, (struct sockaddr*) & serverAddr, sizeof(serverAddr)) < 0) {            //Bind socket
+        cerr << "ERROR: Unable to bind";
+        exit(EXIT_FAILURE);
+    }
+
+    fd_set readfds;                             // File decriptors to check
+    FD_ZERO(&readfds);                          // Set file descriptors to zero
+    FD_SET(sockfd, &readfds);                   // Set the file descriptor 
+
+    addrSize = sizeof(clientAddr);              // Save size
 }
 
 void signal_quit(int quit) { // Function for handling SIGQUIT signal
